8979dba447e4a943f5a6f767680b8cbf83afa137
Suriya50 <suryazzx6@gmail.com>
2026-07-08 20:16:32 +0530

    new change

--- server/routes/chatroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChats, getOrCreateChat } = require('../controllers/chatController');
-const { protect } = require('../middleware/auth');
-
-router.get('/', protect, getChats);
-router.get('/private/:userId', protect, getOrCreateChat);
-
-module.exports = router;
